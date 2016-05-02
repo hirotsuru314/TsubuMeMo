0c3d17f9f41dd93ea1112863d0b797f9f3ddebbb
hirotsuru314 <hirofumi.tsuruta.314@gmail.com>
2016-05-02 14:37:19 +0000

    add edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,44 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "投稿しました！"
-      redirect_to root_url
+      redirect_to root_path
     else
       render 'welcome/home'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      flash[:success] = "投稿を編集しました。"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    if @post.destroy
+      flash[:success] = "投稿を削除しました。"
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
   
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "TsubuMeMoへようこそ！"
-      redirect_to user_path(@user)
+      redirect_to login_path
     else
       render 'new'
     end
